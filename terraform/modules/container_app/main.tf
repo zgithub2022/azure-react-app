6892d0fe4c8206f6eaf5c786799a53abdce3d1ca
Sanjay Baranwal <priyusanj@gmail.com>
2025-04-28 15:18:12 +0100

    feat: resrouce group name corrected

--- terraform/modules/container_app/main.tf
+++ terraform/modules/container_app/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "react_container_app_rg" {
 resource "azurerm_user_assigned_identity" "webapp_id" {
   location            = var.location
   name                = "containerappmi-${var.environment}"
-  resource_group_name = data.azurerm_resource_group.react_container_app_rg.name
+  resource_group_name = azurerm_resource_group.react_container_app_rg.name
     tags = {
      "environment" = var.environment
   }
