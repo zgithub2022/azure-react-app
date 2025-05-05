fc7a3bc59a3ab7e1ef4ed63897adc2726ae72c4a
Sanjay Baranwal <priyusanj@gmail.com>
2025-05-05 21:50:11 +0100

    feat: combine all terraform code

--- terraform/main/variables.tf
+++ terraform/main/variables.tf
@@ -19,3 +19,19 @@ variable "container_registry" {
   description = "Name of container registry"
   type        = string
 }
+
+variable "app_tier" {
+  description = "The tier of app"
+  type        = string
+  default     = "F1"
+}
+
+variable "ip_address_list" {
+  description = "Allowed ip address for app"
+  type        = list(string)
+}
+
+variable "container_port" {
+  description = "container port"
+  type        = number
+}
