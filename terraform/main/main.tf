40558cffa45eca92be6177ca407de0b737a61304
Sanjay Baranwal <priyusanj@gmail.com>
2025-05-05 14:26:33 +0100

    chore: variable corrected

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,11 +1,11 @@
 module "contaier_app" {
-  source               = "./modules/container_app"
-  environment          = var.environment
-  app_azurerm_rg       = var.app_azurerm_rg
-  location             = var.location
-  docker_image_name    = var.docker_image_name
-  acr_name             = var.acr_name
-  app_tier             = var.app_tier
-  ip_address_list      = var.ip_address_list
-  container_port       = var.container_port
+  source            = "./modules/container_app"
+  environment       = var.environment
+  app_azurerm_rg    = var.app_azurerm_rg
+  location          = var.location
+  docker_image_name = var.docker_image_name
+  acr_name          = var.acr_name
+  app_tier          = var.app_tier
+  ip_address_list   = var.ip_address_list
+  container_port    = var.container_port
 }
